6b716dbb6d696dc573281a06cb9dde23a7116d08
lucyjcbsn <berg.feltstykket@icloud.com>
2026-08-30 18:37:11 +0200

    fixed everything and added nothing

--- nchex.s
+++ nchex.s
@@ -2,37 +2,24 @@ section .data
 	;cfg
 	debug_cfg: db 1
 
-	dbg_startmsg: db "Test", 0
-	err1: db "Error, unable to open binary", 0
-	search_err1: db "Address invalid", 0
+	dbg_startmsg: db "test no init", 10, 0
+	;err1: db "Error, unable to open binary", 0
+	;search_err1: db "Address invalid", 0
 
 section .text
 	global main
-	extern interface
-	extern input
-	extern convert
 
 main:
-	mov eax, [debug_cfg]
-	cmp eax, 0
-	jne run_no_startmsg
-	;start routine
+	cmp byte [debug_cfg], 1
+	jne init
+	;test
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, [dbg_startmsg]
-	mov rdx, 5
+	mov rsi, dbg_startmsg
+	mov rdx, 13
+	syscall
 
-	run_no_startmsg:
-		
-	
-	
-
-
-interface:
-		
-input:
-
-convert:
-
-file:
-	
+	init:
+	mov rax, 60
+	mov rdi, 0
+	syscall
