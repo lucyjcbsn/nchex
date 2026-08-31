b3c69787889a77a11001881d724a248167110484
lucyjcbsn <berg.feltstykket@icloud.com>
2026-08-31 22:02:09 +0200

    shit I don't want to fix this

--- nchex.s
+++ nchex.s
@@ -12,28 +12,35 @@ section .text
 main:
 
 	cmp byte [debug_cfg], 1
-	jne init
+	jne read_init
 	;test
+        push rdi
+        push rsi
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, dbg_startmsg
 	mov rdx, 13
 	syscall
+        pop rsi
+        pop rdi
 	
-	init:
-	;DEBUG TIL 37
+read_init:
+	cmp rdi, 2
+        jl no_arg
 
-	xor rax, rax
-		
-	arg_str_read:
-	cmp byte ptr [rsi + rax], 0
+        mov rsi, [rsi + 8]
+        xor rcx, rcx
+
+arg_read:
+	cmp byte [rsi + rax], 0
+	je bin_get
 	inc rcx
-	jne arg_str_read
-	
+	jne arg_read
+
+bin_get:
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, rcx
-	mov rdx, 
+	mov rdx, rcx
 	syscall
 
 	mov rax, 60
