f2662ca1c3ab4a45389a4ae2b8ca65e0a0f17b8b
Lucille Jacobsen <berg.feltstykket@icloud.com>
2026-09-02 09:17:19 +0200

    jævla tab

--- nchex.s
+++ nchex.s
@@ -14,27 +14,27 @@ main:
 	cmp byte [debug_cfg], 1
 	jne read_init
 	;test
-    push rdi
-    push rsi
+	push rdi
+	push rsi
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, dbg_startmsg
 	mov rdx, 13
 	syscall
-    pop rsi
-    pop rdi
+	pop rsi
+	pop rdi
 	
 read_init:
 	cmp rdi, 2
-    jl exit
+	jl exit
 
 	mov rsi, [rsi + 8]
 	;sesame
 	mov rax, 2
-    mov rdi, rsi
-    xor rsi, rsi
-    xor rdx, rdx
-    syscall
+	mov rdi, rsi
+	xor rsi, rsi
+	xor rdx, rdx
+	syscall
 
 	cmp rax, 0
 	jnl read
